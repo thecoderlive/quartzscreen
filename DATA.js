37f42ea2e124dfba2e5f423fc321db288dd80552
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-23 13:17:44 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,33 @@
+export const item = {
+    "id": "0c36b126-269e-4612-8805-5495f5a71320",
+    "title": "send",
+    "object_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png",
+    "about_object": "Knowledge station grow citizen. Her too she ability design contain. Tell during item general.",
+    "ar_detail": [
+        {
+            "id": "8966a30f-6da8-4322-bfd4-044e71716c70",
+            "ar_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png",
+            "about_ar": "Exactly quickly push require third part. Send arm however green draw item."
+        },
+        {
+            "id": "5faef6d4-7401-4afd-af9b-3484cadc27b3",
+            "ar_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png",
+            "about_ar": "Because well when case produce. College movie learn something. Theory wait according even form concern."
+        },
+        {
+            "id": "ec37c294-9b17-4c44-8dea-90874f3b9692",
+            "ar_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png",
+            "about_ar": "Campaign fight plant worry teacher rather simple enter."
+        },
+        {
+            "id": "416958b4-1c5e-407f-93c8-d135af126826",
+            "ar_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png",
+            "about_ar": "Close so information including I. Feeling maintain option hear at talk. Since him rate money."
+        },
+        {
+            "id": "fc67a0e2-ce4a-41aa-8dcc-5db25ae0b785",
+            "ar_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png",
+            "about_ar": "Business majority family this. Program seek tell task ability."
+        }
+    ]
+}
